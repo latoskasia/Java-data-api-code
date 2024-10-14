98624efd201b0ef3dce545efa7e13027e5ae6e60
latoskasia <101577342+latoskasia@users.noreply.github.com>
2024-10-14 14:47:17 +0200

    stage 02 completed, stage 03 also but without docker

--- data-service/src/main/java/com/example/data_service/CustomerRepository.java
+++ data-service/src/main/java/com/example/data_service/CustomerRepository.java
@@ -1,4 +1,8 @@
 package com.example.data_service;
 
-public class CustomerRepository {
+import com.example.data_service.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
 }
--- data-service/src/main/java/com/example/data_service/DataController.java
+++ data-service/src/main/java/com/example/data_service/DataController.java
@@ -1,4 +1,38 @@
 package com.example.data_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/customers")
 public class DataController {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    @PutMapping
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customer){
+        customer.setId(id);
+        return customerRepository.save(customer);
+    }
+
+    @DeleteMapping
+    public void deleteCustomer(@PathVariable Long id){
+        customerRepository.deleteById(id);
+    }
+
+
+
 }
