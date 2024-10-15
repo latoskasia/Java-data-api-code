13c1edaed187f41155a1b3552321e5c79bc91964
latoskasia <101577342+latoskasia@users.noreply.github.com>
2024-10-15 12:09:47 +0200

    first part closed and working - beated database ;)

--- data-service/src/main/java/com/example/data_service/Customer.java
+++ data-service/src/main/java/com/example/data_service/Customer.java
@@ -3,7 +3,7 @@ package com.example.data_service;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="customer")
+@Table(name="customers")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
